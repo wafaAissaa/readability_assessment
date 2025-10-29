0c6a3bf4fa9b6603d58d9168e93907251c70ec99
wafaAissaa <wafaaissa94@gmail.com>
2025-10-29 17:50:02 +0100

    sbatch

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=train-DL-0
+#SBATCH --output="output0.txt"
+#SBATCH --partition=gpu
+#SBATCH --gres="gpu"
+#SBATCH --qos="preemptible"
+#SBATCH --mem-per-cpu=10000
+#SBATCH --ntasks=1
+#SBATCH --constraint=TeslaA100|GeForceRTX3090|TeslaV100
+
+python train.py
